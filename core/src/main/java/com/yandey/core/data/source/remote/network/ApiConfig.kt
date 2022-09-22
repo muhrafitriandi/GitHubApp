8f04a3472fb5b87c22b8d23f4497d91b18625cf8
Muhammad Rafi Triandi <61121146+muhrafitriandi@users.noreply.github.com>
2022-09-23 00:06:03 +0700

    certificate pinning on github api

--- core/src/main/java/com/yandey/core/data/source/remote/network/ApiConfig.kt
+++ core/src/main/java/com/yandey/core/data/source/remote/network/ApiConfig.kt
@@ -1,7 +1,7 @@
 package com.yandey.core.data.source.remote.network
 
-import com.yandey.core.BuildConfig.BASE_URL
-import com.yandey.core.BuildConfig.DEBUG
+import com.yandey.core.BuildConfig.*
+import okhttp3.CertificatePinner
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
@@ -16,10 +16,17 @@ class ApiConfig {
             } else {
                 HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.NONE)
             }
+            val certificatePinner = CertificatePinner.Builder()
+                .add(HOSTNAME, SHA1)
+                .add(HOSTNAME, SHA2)
+                .add(HOSTNAME, SHA3)
+                .add(HOSTNAME, SHA4)
+                .build()
             val client = OkHttpClient.Builder()
                 .addInterceptor(loggingInterceptor)
                 .connectTimeout(120, TimeUnit.SECONDS)
                 .readTimeout(120, TimeUnit.SECONDS)
+                .certificatePinner(certificatePinner)
                 .build()
             val retrofit = Retrofit.Builder()
                 .baseUrl(BASE_URL)
